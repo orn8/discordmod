192f613267f0e2ce9b5d6c9e1645bc4cce9fcd9e
oragne <banananananananabanana311@gmail.com>
2023-12-03 15:08:48 +1100

    fixed it (sorry for delay)

--- Experiments.plugin.js
+++ Experiments.plugin.js
@@ -3,22 +3,26 @@
  * @author oragne
  * @authorId 1101637133850132572
  * @description Enables access to experiments and staff-only options for normal users.
- * @version 1.2.2
+ * @version 1.2.3
  * @source https://github.com/choco705/Experiments-BetterDiscord
  * @updateUrl https://raw.githubusercontent.com/choco705/Experiments-BetterDiscord/main/Experiments.plugin.js
  */
 
 module.exports = class discordExperiments {
   start() {
-    BdApi.showToast("Experiments v1.2.2 Active", {type:"info",icon: true,timeout: 5000,forceShow: true});
+    BdApi.showToast("Experiments v1.2.3 Active", {type:"info",icon: true,timeout: 5000,forceShow: true});
     try {
-      let c = window.webpackChunkdiscord_app.push([[Symbol()],{},({c})=>Object.values(c)]);
-      let u = c.find((x)=> x?.exports?.default?.getUsers).exports.default;
-      let m = Object.values(u._dispatcher._actionHandlers._dependencyGraph.nodes);
-      u.getCurrentUser().flags |= 1;
-      m.find((x)=>x.name === "DeveloperExperimentStore").actionHandler["CONNECTION_OPEN"]();
-      try {m.find((x)=>x.name === "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({user:{flags: 1}})} catch {}
-      m.find((x)=>x.name === "ExperimentStore").storeDidChange()
+      // START
+      let cache; webpackChunkdiscord_app.push([["wp_isdev_patch"], {}, r => cache=r.c]);
+      var UserStore = Object.values(cache).find(m => m?.exports?.default?.getUsers).exports.default;
+      var actions = Object.values(UserStore._dispatcher._actionHandlers._dependencyGraph.nodes);
+      var user = UserStore.getCurrentUser();
+      actions.find(n => n.name === "ExperimentStore").actionHandler.CONNECTION_OPEN({
+      	type: "CONNECTION_OPEN", user: {flags: user.flags |= 1}, experiments: [],
+      });
+      actions.find(n => n.name === "DeveloperExperimentStore").actionHandler.CONNECTION_OPEN();
+      webpackChunkdiscord_app.pop(); user.flags &= ~1; "done";
+      // END
     } catch (err) {
       console.log(err);
       BdApi.showNotice(
